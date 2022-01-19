a146cb73fb90278643143cd0eb83c5a224fce648
KitKat2002 <89272992+KitKat2002@users.noreply.github.com>
2022-01-19 00:07:48 -0700

    Add files via upload

---
+++ pet.py
@@ -0,0 +1,47 @@
+class Pet:
+    name = "Fido"
+    species = "Canine"
+    pID = "43"
+
+    def getPetInfo(self):
+        pet_name = input("Enter your pet's name: ")
+        pet_species = input("Enter your pet's species: ")
+        pet_id = input("Enter your pet's id: ")
+        if (pet_name == self.name and pet_id == self.pID):
+            print("Hello, {}.".format(pet_name))
+        else:
+            print("Pet name or ID is incorrect.")
+
+class Large_Pet(Pet):
+    weight = " "
+    diet = " "
+    temperment = " "
+    brand = " "
+
+    def getPetInfo(self):
+        pet_name = input("Enter your pet's name: ")
+        pet_species = input("Enter your pet's species: ")
+        pet_brand = input("Enter your pet's brand: ")
+        if (pet_name == self.name and pet_brand == self.brand):
+            print("Hello, {}.".format(pet_name))
+        else:
+            print("Pet name or brand is incorrect.")
+
+
+
+class Pet_Diagnosis(Pet):
+    symptoms = " "
+    duration_of_symptoms = " "
+    last_location = " "
+    illness = " "
+
+    def getPetInfo(self):
+        pet_name = input("Enter your pet's name: ")
+        pet_species = input("Enter your pet's species: ")
+        pet_illness = input("Enter your pet's illness: ")
+        if (pet_name == self.name and pet_illness == self.illness):
+            print("Hello, {}.".format(pet_name))
+        else:
+            print("Pet name or illness is incorrect.")
+
+
