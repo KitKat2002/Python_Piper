94b90495d00e1d566ebcfd96e73c9c94862a711d
KitKat2002 <89272992+KitKat2002@users.noreply.github.com>
2022-01-21 22:41:10 -0700

    Update Abstract.py

--- Abstract.py
+++ Abstract.py
@@ -1,11 +1,13 @@
+#Imports abstract method
 from abc import ABC, abstractmethod
 class Bike(ABC):
     def Bill(self, amount):
         print("This month's payment is ",amount)
-
+#Utilizes abstract method
+    @abstractmethod    
     def payment(self, amount):
         pass
-
+#utilizes information from parent class
 class BikeMonthlyPayment(Bike):
     def monthlyPayment(self, amount):
         print('After this month\'s payment of {}, your total remaining due is $500 '.format(amount))
