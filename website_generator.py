7a292ac4e28756789744337d9828043db5e32657
KitKat2002 <89272992+KitKat2002@users.noreply.github.com>
2022-01-26 23:20:34 -0700

    Update website_generator.py

--- website_generator.py
+++ website_generator.py
@@ -4,7 +4,7 @@ import os
 from tkinter import *
 
 #Creates a function holding the webpage format
-def Submit(self):
+def Submit(txtfld):
     text = txtfld.get()
     html_template = """
     <html>
@@ -18,15 +18,17 @@ def Submit(self):
     f = open("BasicWebsite.html", "w")
     f.write(html_template)
     f.close()
+    webbrowser.open_new_tab("BasicWebsite.html")
 
 filename = 'file:///'+os.getcwd()+'/' + 'BasicWebsite.html'
 
 #Creates the gui for changing the webpage body
 window=Tk()
-btn=Button(window, text="To change body click here", fg='brown', command = Submit("webbrowser.open_new_tab(filename)"))
+btn=Button(window, text="To change body click here", fg='brown', command = lambda:Submit(txtfld))
 btn.place(x=80, y=100)
 txtfld=Entry(window, text="", bd=5)
 txtfld.place(x=80, y=150)
 window.title('Webpage Generator')
 window.geometry("300x200+10+10")
 window.mainloop()
+
